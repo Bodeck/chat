afb82c53b4943f8d5722e76fa58570cabaf8991a
Bogus Franczak <bfranczak@gmail.com>
2019-04-27 16:34:45 +0200

    Setup dev environment

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HTMLTemplatePlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+const plugins = [new HTMLTemplatePlugin({
+  template: 'client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})];
+
+module.exports = (env) => {
+  const environment = env;
+  if (environment === 'production') {
+    plugins.push(new OptimizeJsPlugin({
+      sourceMap: false
+    }))
+  }
+  return {
+    mode: environment,
+    entry: './client/index.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'app.bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            plugins: env !== 'production' ? ['react-hot-loader/babel'] : []
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader' },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins,
+    devServer: {
+      proxy: {
+        '/socket.io': {
+          target: 'http://localhost:3000',
+          ws: true
+        }
+      }
+    }
+  }
+};
